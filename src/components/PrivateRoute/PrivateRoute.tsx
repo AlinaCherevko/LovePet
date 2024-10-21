763de1bee7beb6b28035f90e73e32075de42660e
Alina Cherevko <alinapavluhina@ukr.net>
2024-10-21 15:04:30 +0300

    fix path

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,8 +1,8 @@
 import { type FC } from "react";
-import { Route } from "../RestrictedRoute/RestrictedRoute";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { selectIsAuth } from "../../redux/auth/authSelectors";
+import { Route } from "../RestrictedRoute/types";
 
 const PrivateRoute: FC<Route> = ({ children, redirectTo = "/login" }) => {
   const isAuth = useSelector(selectIsAuth);
